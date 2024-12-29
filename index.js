75eec524e103bec59db4fe3f7572df9f50ea4865
Sidthesimp <guapochoy@gmail.com>
2024-12-29 14:33:26 +0800

    Add files via upload
    Basics of javascript

---
+++ index.js
@@ -0,0 +1,20 @@
+console.log('Hello');
+console.log('i like pizza');
+
+alert('This is an alert');
+alert('I like pizza!');
+
+document.getElementById('myHeader').textContent = 'Hello';
+document.getElementById('myP').textContent = 'I like pizza!';
+
+//this is a comment
+
+/*
+    this
+    is
+    a
+    comment
+
+    */
+
+
